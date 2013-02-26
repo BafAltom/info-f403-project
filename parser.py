c1549fa70e32dca7858ce87de415ac3734a7f199
Thomas Chapeaux <chapeauxthomas@gmail.com>
2013-02-26 13:11:12 +0100

    Parser now create a parse tree during parsing Added test cases (including one that fails until our grammar is LL1)

--- parser.py
+++ parser.py
@@ -1,65 +1,120 @@
+class parseTreeNode:
+	def __init__(self, value):
+		self.value = value
+		self.father = None
+		self.children = []
+
+	def giveChild(self, value):
+		child = parseTreeNode(value)
+		child.father = self
+		self.children.append(child)
+
+	def getDepth(self):
+		current_depth = 1
+		current_node = self
+		while (current_node.father):
+			current_node = current_node.father
+			current_depth += 1
+		return current_depth
+
+	def __repr__(self):
+		my_repr = "\t" * self.getDepth()
+		my_repr += self.value + "\n"
+		for child in self.children:
+			my_repr += str(child)
+		return my_repr
+
 class LL1Parser:
-	def __init__(self, grammar):
+	def __init__(self, grammar, verbose=False):
 		self.grammar = grammar
 		self.M = grammar.actionTable()
 
-		self.stack = []
+		self.endSymbol = "__UNDEFINED"
+
 		self.input = None
-		self.output = None
+
+		self.parseTree = None
+		self.currentNode = None
+
+		self.verbose = verbose
 		self.error = False
 		self.success = False
+		self.output = None
 
 	def parse(self, inputText, endSymbol="$"):
+
 		self.input = inputText  # list of symbols
+		self.endSymbol = endSymbol
 		self.output = []
 		self.error = False
 		self.success = False
 
-		self.stack.append(self.grammar.startSymbol)
+		self.parseTree = parseTreeNode(self.grammar.startSymbol)
+		self.currentNode = self.parseTree
 
-		while (not self.error and not self.success):
-			# print "-----"
-			# print "stack", self.stack
-			# print "input", self.input
-			# print "output", self.output
-			x = self.stack[-1]
-			u = self.input[0]  # only one character as we have an LL1-parser
-			# print "x", x
-			# print "u", u
-			if (x in self.grammar.symbols and u in self.M[x]):
-				self.produce(self.M[x][u])
-			elif (x in self.grammar.terminals and x != endSymbol):
-				self.match()
-			elif (x == u and x == '$'):
-				self.trigger_accept()
-			else:  # Not expected
-				self.trigger_error()
+		self.parse_recursiveCall()
 		return self.output
+
+	def parse_recursiveCall(self):
+		if (self.error):
+			return
+
+		if (self.verbose):
+			print "-----"
+			print "input", self.input
+			print "output", self.output
+			print "parseTree :\n", self.parseTree
+			print "currentNode", self.currentNode.value
+		x = self.currentNode.value
+		u = self.input[0]  # only one character as we have an LL1-parser
+
+		if (self.verbose):
+			print "x", x
+			print "u", u
+
+		if (x in self.grammar.symbols and u in self.M[x]):
+			self.produce(self.M[x][u])
+		elif (x in self.grammar.terminals and x != self.endSymbol):
+			self.match()
+		elif (x == u and x == '$'):
+			self.trigger_accept()
+		else:  # Not expected
+			self.trigger_error()
 
 	def trigger_error(self):
 		self.output.append("E")
+
+		if (self.verbose):
+			print "------ error"
+			print "currentNode", self.currentNode.value
+			print "input", self.input
+			print "output", self.output
+
 		self.error = True
-		# print "------ error"
-		# print "stack", self.stack
-		# print "input", self.input
-		# print "output", self.output
 
 	def trigger_accept(self):
 		self.output.append("A")
 		self.success = True
-		# print("accept")
+		if (self.verbose):
+				print "accept"
 
 	def produce(self, i):
-		# print "produce", i
+		if (self.verbose):
+			print "produce", i
 		self.output.append("P" + str(i))
-		self.stack.pop()
-		for produced in reversed(self.grammar.rules[i][1:]):
+		saved_current = self.currentNode
+		for produced in self.grammar.rules[i][1:]:
 			if (produced != self.grammar.emptySymbol):
-				self.stack.append(produced)
+				self.currentNode.giveChild(produced)
+				self.currentNode = self.currentNode.children[-1]
+				self.parse_recursiveCall()
+				self.currentNode = saved_current
+		self.currentNode = saved_current
 
 	def match(self):
-		# print "match"
-		a = self.stack.pop()
+		if (self.verbose):
+			print "match"
+		a = self.currentNode.value
 		b = self.input.pop(0)
 		assert a == b, "match : input and stack does not match"
 		self.output.append("M")
--- parser_test.py
+++ parser_test.py
@@ -1,7 +1,26 @@
-import cfgrammar
 import parser
 
 def test_1():
+	from grammars_examples import g1
+	ll1_parser = parser.LL1Parser(g1)
+
+	inputString = ["begin", "Id", ":=", "Id", "+", "Nb", ";", "write", "(", "Id", "+", "(", "Nb", "-", "Id", ")", ")", ";", "end", "$"]
+
+	out = ll1_parser.parse(inputString)
+	assert out[-1] == 'A'
+
+#                                              v unknown symbol
+	inputString = ["begin", "Id", ":=", "Id", "*", "Nb", ";", "write", "(", "Id", "+", "(", "Nb", "-", "Id", ")", ")", ";", "end", "$"]
+
+	out = ll1_parser.parse(inputString)
+	assert out[-1] == 'E'
+
+	inputString = ["begin", "Id", ":=", "Id", "+", "Nb", ";", "write", "(", "Id", "+", "(", "Nb", "-", "Id", ")", ";", "end", "$"]
+
+	out = ll1_parser.parse(inputString)
+	assert out[-1] == 'E'
+
+def test_2():
 	from grammars_examples import g2
 	ll1_parser = parser.LL1Parser(g2)
 
@@ -13,7 +32,7 @@ def test_1():
 	out = ll1_parser.parse(inputString)
 	assert out[-1] == 'E'
 
-def test_2():
+def test_3():
 	from grammars_examples import g3
 	ll1_parser = parser.LL1Parser(g3)
 
@@ -23,3 +42,4 @@ def test_2():
 
 test_1()
 test_2()
+test_3()
