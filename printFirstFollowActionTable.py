4a4eed75f1ed63a844ee28cf6c0200be9a0ca42a
Thomas Chapeaux <chapeauxthomas@gmail.com>
2013-03-15 18:07:42 +0100

    Added script to print First, Follow & AT in a readable form (easily copy-pastable to LibreOffice Calc)

---
+++ printFirstFollowActionTable.py
@@ -0,0 +1,41 @@
+from grammars_examples import g6
+
+print "first"
+firstString = ""
+first = g6.first_1()
+for a, b in first.items():
+	if a not in g6.terminals:
+		firstString += str(a) + " : "
+		for termin in b:
+			firstString += str(termin) + ", "
+		firstString += "\n"
+print firstString
+
+print "follow"
+followString = ""
+follow = g6.follow_1()
+for a, b in follow.items():
+	if a not in g6.terminals:
+		followString += str(a) + " : "
+		for termin in b:
+			followString += str(termin) + ", "
+		followString += "\n"
+print followString
+
+print "actionTable"
+aT = g6.actionTable()
+symbols = g6.symbols
+terminals = g6.terminals
+aTString = " , "
+for t in terminals:
+	aTString += str(t) + ", "
+aTString += "\n"
+for a in symbols:
+	if a not in g6.terminals:
+		aTString += str(a) + ", "
+		for b in terminals:
+			if b in aT[a]:
+				aTString += str(aT[a][b])
+			aTString += ", "
+		aTString += "\n"
+print aTString
